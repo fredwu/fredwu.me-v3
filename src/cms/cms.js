880d5e76ebd2ab5b6d680f172350ba213ee02f30
Fred Wu <ifredwu@gmail.com>
2019-08-30 15:52:33 +1000

    Updated all the packages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,8 @@
-import CMS from 'netlify-cms'
+import CMS from 'netlify-cms-app'
 
 import PagePreview from './preview-templates/PagePreview'
 import PostPreview from './preview-templates/PostPreview'
 
+CMS.init()
 CMS.registerPreviewTemplate('about', PagePreview)
 CMS.registerPreviewTemplate('blog', PostPreview)
