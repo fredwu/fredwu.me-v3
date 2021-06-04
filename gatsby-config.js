86c81dfb15ae9088003377da7373530df7715ec9
Fred Wu <ifredwu@gmail.com>
2021-06-04 15:01:25 +1000

    `serialize` is needed for rss :facepalm:

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,6 +96,17 @@ module.exports = {
         `,
         feeds: [
           {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
             query: `
               {
                 allMarkdownRemark(
