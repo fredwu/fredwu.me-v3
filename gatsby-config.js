58f4891675ef4b0728a4e27873be6eaeef13ec77
Fred Wu <ifredwu@gmail.com>
2023-04-22 02:29:59 +1000

    Use the new gtags plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,44 @@
 module.exports = {
   siteMetadata: {
-    title: 'Fred Wu - Engineering, Design, Photography, Leadership',
+    title: "Fred Wu - Engineering, Design, Photography, Leadership",
     description:
-      'Located in Melbourne, Australia, I am a software developer and leader with a strong focus on code craftsmanship, business value and user experience.',
-    siteUrl: 'https://fredwu.me',
+      "Located in Melbourne, Australia, I am a software developer and leader with a strong focus on code craftsmanship, business value and user experience.",
+    siteUrl: "https://fredwu.me",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sass",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
+        name: "uploads",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
+        name: "images",
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
@@ -48,34 +48,34 @@ module.exports = {
             },
           },
           {
-            resolve: 'gatsby-remark-embed-video',
+            resolve: "gatsby-remark-embed-video",
             options: {
               related: false,
               noIframeBorder: true,
             },
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
+              destinationDir: "static",
             },
           },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: 'language-',
+              classPrefix: "language-",
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,
               noInlineHighlight: true,
             },
           },
-          'gatsby-remark-reading-time',
+          "gatsby-remark-reading-time",
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `
           {
@@ -98,9 +98,9 @@ module.exports = {
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                })
-              })
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
             },
             query: `
               {
@@ -121,37 +121,59 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
-            match: '^/blog/',
+            output: "/rss.xml",
+            match: "^/blog/",
             title: "Fred Wu's Blog Feed",
           },
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-purgecss',
+      resolve: "gatsby-plugin-purgecss",
       options: {
         develop: true,
-        purgeOnly: ['/all.scss'],
+        purgeOnly: ["/all.scss"],
       },
     }, // must be after other CSS plugins
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingId: 'UA-13137273-1',
-        head: false,
-        exclude: ['/preview/**', '/admin/**'],
-        cookieDomain: 'fredwu.me',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6JXLK9EZ1M", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
       },
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        policy: [{ userAgent: '*', disallow: '/admin' }],
+        policy: [{ userAgent: "*", disallow: "/admin" }],
       },
     },
-    'gatsby-remark-responsive-iframe',
-    'gatsby-redirect-from',
-    'gatsby-plugin-sitemap',
+    "gatsby-remark-responsive-iframe",
+    "gatsby-redirect-from",
+    "gatsby-plugin-sitemap",
   ],
-}
+};
